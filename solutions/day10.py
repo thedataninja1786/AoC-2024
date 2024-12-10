1feee3852e9308a0746adc190ebb6ce3fd9ec489
thedataninja1786 <chrissolomou1786@gmail.com>
2024-12-10 13:01:48 +0200

    Day 10

--- solutions/day10.py
+++ solutions/day10.py
@@ -11,26 +11,20 @@ for i in range(len(grid)):
     
 # <-------------------------------------------------------  PART 1  -------------------------------------------------------> 
 def dfs(i,j,tmp,paths):
-    if tmp and tmp[-1] == 9 and (i,j) not in global_visited:
-        global_visited.add((i,j))
+    if tmp and tmp[-1] == 9 and (i,j) not in visited:
+        visited.add((i,j))
         res.add(tuple(paths))
         return
-
-    if (i,j) in visited:
-        return 
-    visited.add((i,j))
 
     coords = [[1,0], [-1,0], [0,1], [0,-1]]
     for c in coords:
         x = i + c[0]
         y = j + c[1]
-        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and (x,y) not in visited and tmp and grid[x][y] - 1 == tmp[-1]:
+        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and tmp and grid[x][y] - 1 == tmp[-1]:
             dfs(x,y,tmp + [grid[x][y]],paths + [(x,y)])
 
-global_visited = set()
 s = 0 
 for i,j in starts:
-    global_visited = set()
     visited = set() 
     res = set()
     dfs(i,j,[grid[i][j]],[(i,j)])
