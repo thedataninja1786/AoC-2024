08808912803af4159c6f2f09167bcb80d8702414
thedataninja1786 <chrissolomou1786@gmail.com>
2024-12-12 23:32:18 +0200

    Day 12 - Part 1

--- solutions/day12.py
+++ solutions/day12.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
+# Plants of the same type can appear in multiple separate regions, and regions can even appear within other regions.
+
 with open("data/day12.txt","r") as f:
     data = f.read()
 
 grid = [list(x) for x in data.splitlines()]
-
-# Plants of the same type can appear in multiple separate regions, and regions can even appear within other regions.
 
 def process_grid(grid):
     def bfs(i,j,n):
@@ -37,16 +37,27 @@ def process_grid(grid):
             bfs(i,j,n)
             t = str(n)
             visited.add((i,j))
-
-
-    c = defaultdict(list)
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            c[grid[i][j]].append((i,j))
-
     return grid
 
 grid = process_grid(grid)
+
+def find_bounds(point_loc):
+    # finds the coordinates where the region starts and ends
+    coords = defaultdict(list)
+    for k,v in point_loc.items():
+        mn = min([x[0] for x in v])
+        mx = max([x[0] for x in v])
+        tmp = [x for x in v if x[0] == mn]
+        top_left = sorted(tmp,key = lambda x: x[1])[0]
+        top_right = sorted(tmp,key = lambda x: x[1],reverse=True)[0]
+
+        tmp = [x for x in v if x[0] == mx]
+        bottom_left = sorted(tmp,key = lambda x:x[1])[0]
+        bottom_right = sorted(tmp,key=lambda x:x[1],reverse=True)[0]
+
+        coords[k] = [[top_left,bottom_left,top_right,bottom_right]]
+    
+    return coords
 
 
 # part 1
@@ -56,21 +67,6 @@ for i in range(len(grid)):
     for j in range(len(grid[0])):
         point_loc[grid[i][j]].append((i,j))
 
-
-# find the points where the region starts and ends
-coords = defaultdict(list)
-for k,v in point_loc.items():
-    mn = min([x[0] for x in v])
-    mx = max([x[0] for x in v])
-    tmp = [x for x in v if x[0] == mn]
-    top_left = sorted(tmp,key = lambda x: x[1])[0]
-    top_right = sorted(tmp,key = lambda x: x[1],reverse=True)[0]
-
-    tmp = [x for x in v if x[0] == mx]
-    bottom_left = sorted(tmp,key = lambda x:x[1])[0]
-    bottom_right = sorted(tmp,key=lambda x:x[1],reverse=True)[0]
-
-    coords[k] = [[top_left,bottom_left,top_right,bottom_right]]
 
 def find_area_perim(k):
     a = len(point_loc[k])
@@ -99,35 +95,52 @@ for k,_ in point_loc.items():
 print(s)
 
 # part 2
-data = """AAAAAA
-AAABBA
-AAABBA
-ABBAAA
-ABBAAA
-AAAAAA"""
+data = """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"""
 
 grid = [list(x) for x in data.splitlines()]
 
 grid = process_grid(grid)
 
-c = defaultdict(list)
+point_loc = defaultdict(list)
 for i in range(len(grid)):
     for j in range(len(grid[0])):
-        c[grid[i][j]].append((i,j))
+        point_loc[grid[i][j]].append((i,j))
 
-def estimate(k):
-    cnt = 0 
-    ls = c[k]
-    print(ls)
-    a = max(1,len(ls))
-    dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-    for i,j in ls:
-        s = 4
-        for d in dirs:
-            x = i + d[0]
-            y = j + d[1]
-            if (0 <= x < len(grid) and 0 <= y < len(grid[0])) and (x,y) in ls:
-                s -= 1
-        cnt += s
-    return a,cnt
+for l in grid:
+    print(l)
 
+def find_edges(point_loc,k):
+    edges = []
+    points = point_loc[k]
+    dirs = [[0,1],[0-1]]
+    for i,j in points:
+        s = 0
+        if j - 1 >= 0 and (i,j-1) in points:
+            s += 1
+        if j + 1 < len(grid[0]) and (i,j+1) in points:
+            s += 1
+        
+        if s < 2:
+            edges.append((i,j))
+    
+    return edges
+
+edges = {}
+for k in point_loc.keys():
+    edges[k] = find_edges(point_loc,k)
+
+cnt = 0
+for num,points in point_loc.items():
+    y_edges = [x[1] for x in edges[num]]
+    print(y_edges)
+    for point in points:
+        if point[1] in y_edges:
+            cnt +=1
+    
+    break
+print(edges)
+print(cnt)
